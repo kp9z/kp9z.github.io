173c121b4e31c5d4a5718499d0419b2db2fd8545
Kenny <anhktrinh@gmail.com>
2022-06-08 13:06:55 +0700

    remove loging

--- bookmarklets/editCoupon.js
+++ bookmarklets/editCoupon.js
@@ -2,8 +2,6 @@
     let link = window.location.href
     const pattern = /(store-coupons).+\/(\d+)/
     let coupon = link.match(pattern)[2]
-    // console.log(coupon)
-    // alert(coupon)
     window.open("https://couponlawn.com/wp-admin/post.php?post="+coupon+"&action=edit")
  
 })()
