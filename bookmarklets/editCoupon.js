b2d97a8f8895e6408a016344ec63515f0e3d38f1
Kenny <anhktrinh@gmail.com>
2022-06-08 12:58:21 +0700

    update logic for filter only coupon-store

--- bookmarklets/editCoupon.js
+++ bookmarklets/editCoupon.js
@@ -1,8 +1,9 @@
 (function(){
     let link = window.location.href
-    const pattern = /(\d+)/g
-    let coupon = link.match(pattern)
-    alert(coupon)
+    const pattern = /(store-coupons).+\/(\d+)/
+    let coupon = link.match(pattern)[2]
+    // console.log(coupon)
+    // alert(coupon)
     window.open("https://couponlawn.com/wp-admin/post.php?post="+coupon+"&action=edit")
  
 })()
