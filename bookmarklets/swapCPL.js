e0dcb864d927cad3213968dfbe43817dd5f4d2bd
Kenny <anhktrinh@gmail.com>
2022-08-17 10:46:28 +0700

    changing https on cl local

--- bookmarklets/swapCPL.js
+++ bookmarklets/swapCPL.js
@@ -1,6 +1,6 @@
 (function(){
     let link = window.location.href
-    const new_link = link.replace('https://couponlawn.com/','http://cl.local/')
+    const new_link = link.replace('https://couponlawn.com/','https://cl.local/')
     window.open(new_link)
  
 })()
