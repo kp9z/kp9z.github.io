035b1c014b1e33356ff4c6671fd63320c2e2d043
Kenny <anhktrinh@gmail.com>
2022-09-23 21:59:24 +0700

    fix error

--- bookmarklets/swapCPL.js
+++ bookmarklets/swapCPL.js
@@ -1,12 +1,11 @@
 (function(){
     let link = window.location.href
     if (link.includes('couponlawn.com/')) {
-        const new_link = link.replace('couponlawn.com/','cl.local/')
+        new_link = link.replace('couponlawn.com/','cl.local/')
     } else {
-        const new_link = link.replace('cl.local/','couponlawn.com/')
+        new_link = link.replace('cl.local/','couponlawn.com/')
 
     }
-
     window.open(new_link)
  
 })()
