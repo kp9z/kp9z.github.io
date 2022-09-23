45bab7a6023e2e156511ea6912d69395caa0b8ea
Kenny <anhktrinh@gmail.com>
2022-09-23 22:00:28 +0700

    update var error

--- bookmarklets/swapCPL.js
+++ bookmarklets/swapCPL.js
@@ -1,11 +1,13 @@
 (function(){
     let link = window.location.href
     if (link.includes('couponlawn.com/')) {
-        new_link = link.replace('couponlawn.com/','cl.local/')
+        const new_link = link.replace('couponlawn.com/','cl.local/')
+        window.open(new_link)
     } else {
-        new_link = link.replace('cl.local/','couponlawn.com/')
+        const new_link = link.replace('cl.local/','couponlawn.com/')
+        window.open(new_link)
 
     }
-    window.open(new_link)
+    
  
 })()
